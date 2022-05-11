d46ada5986532873167be2ef7d183ecad9e1af87
Ujjwal Rajput <33004986+ujjwalRajputGit@users.noreply.github.com>
2022-05-11 09:52:37 +0530

    level added

--- assets/scripts/global/LevelData.ts
+++ assets/scripts/global/LevelData.ts
@@ -1,6 +1,10 @@
 export const LevelData = {
     1: {
         bricks: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        destroyableBricks: 1
+        destroyableBricks: 12
+    },
+    2: {
+        bricks: [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        destroyableBricks: 12
     }
 }
